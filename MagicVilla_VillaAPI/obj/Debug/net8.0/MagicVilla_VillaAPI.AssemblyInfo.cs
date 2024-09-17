36f1aa68eb7aa112f2d38901515bece504988679
DevAlberto <beltonmenete@hotmail.com>
2024-09-17 18:43:16 +0200

    Changes

--- MagicVilla_VillaAPI/obj/Debug/net8.0/MagicVilla_VillaAPI.AssemblyInfo.cs
+++ MagicVilla_VillaAPI/obj/Debug/net8.0/MagicVilla_VillaAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla_VillaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6de8388418a031e2edbeb03704a8f74b931bff12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d8801047aa327863dbe5a1830351527e64816dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla_VillaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla_VillaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
